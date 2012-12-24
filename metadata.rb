9049a2c64a9ec7571543ebd44e3f9e24464beded
Harry Yeh <harryyeh@cometcomputing.com>
2012-12-24 02:48:09 -0800

    Update to v1.0.0

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Comet Computing"
+maintainer_email "devops@cometcomputing.com"
+license          "Apache 2.0"
+description      "Sets the IP Address in Chef Node"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
---
+++ recipes/default.rb
@@ -0,0 +1,24 @@
+#Check to see if this is from the bootstrap of the VM or if this is an individual run list
+nodename = node[:set_hostname]
+if nodename == nil
+	nodename = node[:hostname]
+end
+
+db = data_bag_item("servers",nodename)
+interfaces = db['interfaces']
+
+
+service "networking" do
+	service_name "networking"
+  action :restart
+end
+
+template "/etc/network/interfaces" do
+	source "interfaces.erb"
+	owner "root"	
+	group "root"
+  mode "0644"
+  action :create
+	variables :interfaces => interfaces
+	#notifies :start, "service[networking]", :immediately
+end
