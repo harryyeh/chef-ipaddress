3e850d8cf18e025e5c1238c9ef94ae54ba1ab430
Tim Terhorst <mynamewastaken@gmail.com>
2015-05-03 16:00:56 -0500

    Fixing some style warnings from rubocop.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'ipaddress'
-maintainer       "Comet Computing"
-maintainer_email "devops@cometcomputing.com"
-license          "Apache 2.0"
-description      "Sets the IP Address in Chef Node"
+maintainer       'Comet Computing'
+maintainer_email 'devops@cometcomputing.com'
+license          'Apache 2.0'
+description      'Sets the IP Address in Chef Node'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          '1.2.0'
 supports         'ubuntu', '= 12.04'
