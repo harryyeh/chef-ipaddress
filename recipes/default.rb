e87b77e64f4a9f21360164a4628e6b4bf4b3babc
Tim Terhorst <mynamewastaken@gmail.com>
2015-05-03 00:24:31 -0500

    Add centos template, update recipe to use it.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,24 +1,45 @@
 #Check to see if this is from the bootstrap of the VM or if this is an individual run list
-nodename = node[:set_hostname]
+nodename = node['set_hostname']
 if nodename == nil
-	nodename = node[:hostname]
+    nodename = node['hostname']
 end
 
 db = data_bag_item(node['chef_ipaddress']['databag'], nodename)
 interfaces = db['interfaces']
 
-service "networking" do
-  provider Chef::Provider::Service::Upstart
-	service_name "networking"
-  action :nothing
+if platform_family?('debian')
+    service "networking" do
+        provider Chef::Provider::Service::Upstart
+        service_name "networking"
+        action :nothing
+    end
+
+    template "/etc/network/interfaces" do
+        source "interfaces.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+        action :create
+        variables :interfaces => interfaces
+        notifies :restart, "service[networking]"
+    end
 end
 
-template "/etc/network/interfaces" do
-	source "interfaces.erb"
-	owner "root"	
-	group "root"
-  mode "0644"
-  action :create
-	variables :interfaces => interfaces
-	notifies :restart, "service[networking]"
+if platform_family?('rhel')
+    service 'network' do
+        provider Chef::Provider::Service::Systemd
+        action :nothing
+    end
+
+    interfaces.each do |inter|
+        template "/etc/sysconfig/network-scripts/ifcfg-#{inter}" do
+            source 'ifcfg-interface.erb'
+            owner 'root'
+            group 'root'
+            mode '0644'
+            action :create
+            variables :interfaces => interfaces
+            notifies :restart, 'service[network]'
+        end
+    end
 end
