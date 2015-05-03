6cf50d9a926cc8c84a79e7ae2e36ce5a3f1670f2
Tim Terhorst <mynamewastaken@gmail.com>
2015-05-02 23:24:29 -0500

    Update metadata with supported OS.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "Apache 2.0"
 description      "Sets the IP Address in Chef Node"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.0"
-
+supports         'ubuntu', '= 12.04'
