38e2faa0572ee476170bc5629046cea53614e86b
Tim Terhorst <mynamewastaken@gmail.com>
2015-05-03 21:39:20 -0500

    Fixed tests and platform differentiation.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,45 +1,43 @@
-#Check to see if this is from the bootstrap of the VM or if this is an individual run list
-nodename = node['set_hostname']
-if nodename == nil
-    nodename = node['hostname']
-end
+# Check to see if this is from the bootstrap of the VM,
+# or if this is an individual run list
+nodename = node['set_hostname'].nil? ? node['hostname'] : node['set_hostname']
 
 db = data_bag_item(node['chef_ipaddress']['databag'], nodename)
 interfaces = db['interfaces']
 
 if platform_family?('debian')
-    service "networking" do
-        provider Chef::Provider::Service::Upstart
-        service_name "networking"
-        action :nothing
-    end
+  service 'networking' do
+    provider Chef::Provider::Service::Upstart
+    action :nothing
+  end
 
-    template "/etc/network/interfaces" do
-        source "interfaces.erb"
-        owner "root"
-        group "root"
-        mode "0644"
-        action :create
-        variables :interfaces => interfaces
-        notifies :restart, "service[networking]"
-    end
+  template '/etc/network/interfaces' do
+    source 'interfaces.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+    variables interfaces: interfaces
+    notifies :restart, 'service[networking]'
+  end
 end
 
 if platform_family?('rhel')
-    service 'network' do
-        provider Chef::Provider::Service::Systemd
-        action :nothing
-    end
+  service 'network' do
+    provider Chef::Provider::Service::Systemd
+    action :nothing
+  end
 
-    interfaces.each do |inter|
-        template "/etc/sysconfig/network-scripts/ifcfg-#{inter}" do
-            source 'ifcfg-interface.erb'
-            owner 'root'
-            group 'root'
-            mode '0644'
-            action :create
-            variables :interfaces => interfaces
-            notifies :restart, 'service[network]'
-        end
+  # Centos has a file per interface.
+  interfaces.each do |inter|
+    template "/etc/sysconfig/network-scripts/ifcfg-#{inter.first}" do
+      source 'ifcfg-interface.erb'
+      owner 'root'
+      group 'root'
+      mode '0644'
+      action :create
+      variables interfaces: interfaces
+      notifies :restart, 'service[network]'
     end
+  end
 end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,24 +1,62 @@
 require 'chefspec'
+ChefSpec::Coverage.start!
 
 describe 'ipaddress::default' do
-    let (:chef_run) {
-        ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04')
-        .converge('ipaddress::default')
-    }
+  before do
+    stub_data_bag_item('servers', 'Fauxhai').and_return(
+      id: 'Fauxhai',
+      interfaces: {
+        eth0: {
+          address: '10.1.1.1',
+          netmask: '255.255.255.0',
+          gateway: '10.1.1.255'
+        }
+      }
+    )
+  end
 
-    before do
-        stub_data_bag_item('servers', 'Fauxhai').and_return('Fauxhai')
+  let(:centos_tmpl) { chef_run.template('/etc/sysconfig/network-scripts/ifcfg-eth0') }
+  let(:ubuntu_tmpl) { chef_run.template('/etc/network/interfaces') }
+
+  context 'centos' do
+    let (:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: '7.0')
+        .converge('ipaddress::default')
     end
 
     it 'should run the default recipe' do
-        expect(chef_run).to include_recipe('ipaddress::default')
+      expect(chef_run).to include_recipe('ipaddress::default')
+    end
+
+    it 'should create centos network template' do
+      expect(chef_run).to create_template('/etc/sysconfig/network-scripts/ifcfg-eth0')
+        .with(
+          owner: 'root',
+          group: 'root',
+          mode:  '0644'
+      )
+
+      expect(centos_tmpl).to notify('service[network]').to(:restart)
+    end
+  end
+
+  context 'ubuntu' do
+    let (:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04')
+        .converge('ipaddress::default')
+    end
+
+    it 'should run the default recipe' do
+      expect(chef_run).to include_recipe('ipaddress::default')
     end
 
     it 'should create ubuntu network template' do
-        expect(chef_run).to render_file('/etc/network/interfaces')
+      expect(chef_run).to create_template('/etc/network/interfaces').with(
+        owner: 'root',
+        group: 'root',
+        mode:  '0644'
+      )
+      expect(ubuntu_tmpl).to notify('service[networking]').to(:restart)
     end
-
-    #it 'should create centos network template' do
-    #    expect(chef_run).to render_file('/tmp/derp')
-    #end
+  end
 end
